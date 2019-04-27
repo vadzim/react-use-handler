f96699a69ef8ae0384bc17389c481392914dda41
Vadzim Zieńka <v@vadzim.info>
2019-04-27 09:54:18 +0300

    intial useHandler implementation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 // @flow
 
-/**
- * This function says hello.
- * @param name Some name to say hello for.
- * @returns The hello.
- */
-const sayHello = (name: string = "Haz"): string => `Hello, ${name}!`;
+import { useRef } from "react"
 
-export default sayHello;
+/**
+ * This hook makes a proxy for a function.
+ * It guarantees to return the same instance across multiple renders.
+ * @param f Some recreatable function to wrap in.
+ * @returns function.
+ */
+export function useHandler<F: Function>(f: F): F {
+	const ref = useRef(f)
+	ref.current = f
+	const { current: ret } = useRef(function(...args) {
+		return ref.current.apply(this, args)
+	})
+	return (ret: any)
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,10 @@
-import sayHello from "../src";
+// import { useHandler } from "../src"
 
-test("sayHello", () => {
-  expect(sayHello()).toBe("Hello, Haz!");
-  expect(sayHello("foo")).toBe("Hello, foo!");
-});
+test("no tests here" /* "useHandler should return the same instance" */, () => {
+	// const f = useHandler(x => x)
+	// expect(f(2)).toBe(2)
+	// expect(f).toBe(useHandler(x => x * 2))
+	// expect(f(2)).toBe(4)
+	// expect(f).toBe(useHandler(x => x * 3))
+	// expect(f(2)).toBe(6)
+})
