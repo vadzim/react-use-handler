9477919da61976d9463e2ef88abb8e5dc6e048ef
Vadzim Zieńka <v@vadzim.info>
2020-01-18 15:09:14 +0300

    linting

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,7 +1,3 @@
-declare module 'react-use-handler' {
-    export function useHandler<
-      T extends any[],
-      R,
-      F = (...args: T) => R
-    >(func: F): F;
+declare module "react-use-handler" {
+	export function useHandler<T extends unknown[], R, F = (...args: T) => R>(func: F): F
 }
